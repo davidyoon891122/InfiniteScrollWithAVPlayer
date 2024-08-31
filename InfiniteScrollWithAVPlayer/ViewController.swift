e1b97135b65d3b1f59eb2c4b40ab239cca91ca1c
David Yoon <qqpo12@naver.com>
2024-08-31 17:05:51 +0900

    Initial Commit

---
+++ InfiniteScrollWithAVPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteScrollWithAVPlayer
+//
+//  Created by Jiwon Yoon on 8/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
